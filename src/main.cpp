5750f506ce574c62a982da79d6904526b945500b
Carlos Vieira <vieiramecarlos@gmail.com>
2019-03-15 21:35:15 -0300

    Get scene file as cmd line arg

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include "Rasterizer.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc < 2) {
+		std::cerr << "Invalid number of arguments: ";
+		std::cerr << "scene configuration filename"\
+		" must follow executable name\n";
+		return EXIT_FAILURE;
+	}
+
 	Rasterizer cobra;
-	cobra.load_scene("../big.yaml");
+	std::string scene_file(argv[1]);
+
+	std::cout << "loading scene from " << scene_file << "\n";
+	cobra.load_scene(scene_file);
 	std::cout << "loaded scene\n";
 
+	std::cout << "rasterizing...\n";
 	cobra.rasterize();
 	std::cout << "rasterized\n";
 
+	std::cout << "writing image " << cobra.out_file << "\n";
 	cobra.write_image();
 	std::cout << "done\n";
+
+	return EXIT_SUCCESS;
 }
